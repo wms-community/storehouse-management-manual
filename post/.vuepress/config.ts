8776da4abe7b312ee689ef8752dcf20b1e173251
喵千寻 <101342651+Cinnamoroll-Rabbit@users.noreply.github.com>
2022-05-14 22:31:44 +0800

    Update and rename config.js to config.ts

---
+++ post/.vuepress/config.ts
@@ -0,0 +1,138 @@
+import { viteBundler } from '@vuepress/bundler-vite'
+import { webpackBundler } from '@vuepress/bundler-webpack'
+import { defineUserConfig } from '@vuepress/cli'
+import { docsearchPlugin } from '@vuepress/plugin-docsearch'
+import { googleAnalyticsPlugin } from '@vuepress/plugin-google-analytics'
+import { registerComponentsPlugin } from '@vuepress/plugin-register-components'
+import { shikiPlugin } from '@vuepress/plugin-shiki'
+import { defaultTheme } from '@vuepress/theme-default'
+import { path } from '@vuepress/utils'
+import { navbar, sidebar } from './configs'
+
+const isProd = process.env.NODE_ENV === 'production'
+
+export default defineUserConfig({
+  base: '/',
+
+  locales: {
+    '/': {
+      lang: 'zh-CN',
+      title: 'Storehouse Management 用户手册',
+      description: 'Storehouse Management 用户手册',
+    },
+    '/en-US/': {
+      lang: 'en-US',
+      title: 'Storehouse Management Manual',
+      description: 'Storehouse Management Manual',
+    },
+  },
+
+  bundler:
+    process.env.DOCS_BUNDLER === 'webpack' ? webpackBundler() : viteBundler(),
+
+  theme: defaultTheme({
+    repo: 'wms-community/storehouse-management-manual',
+    docsDir: 'post',
+    locales: {
+      '/': {
+        navbar: navbar.zh,
+        selectLanguageName: '简体中文',
+        selectLanguageText: '选择语言',
+        selectLanguageAriaLabel: '选择语言',
+      },
+
+      '/en-us/': {
+        navbar: navbar.en,
+        sidebar: sidebar.en,
+        editLinkText: 'Edit this page on GitHub',
+        sidebar: sidebar.zh,
+
+        editLinkText: '帮助我们完善这个页面',
+        lastUpdatedText: '最后更新',
+        contributorsText: '贡献者',
+
+        tip: '提示',
+        warning: '警告',
+        danger: '危险',
+
+        notFound: [
+          '这里什么都没有',
+          '我们怎么到这来了？',
+          '这看起来是一个 404 页面',
+          '看起来我们进入了错误的链接',
+        ],
+        backToHome: '返回首页',
+        openInNewWindow: '在新窗口打开',
+        toggleDarkMode: '切换夜间模式',
+        toggleSidebar: '切换侧边栏',
+      },
+    },
+
+    themePlugins: {
+      git: isProd,
+      prismjs: !isProd,
+    },
+  }),
+
+  plugins: [
+    docsearchPlugin({
+      appId: '34YFD9IUQ2',
+      apiKey: '9a9058b8655746634e01071411c366b8',
+      indexName: 'vuepress',
+      searchParameters: {
+        facetFilters: ['tags:v2'],
+      },
+      locales: {
+        '/zh/': {
+          placeholder: '搜索文档',
+          translations: {
+            button: {
+              buttonText: '搜索文档',
+              buttonAriaLabel: '搜索文档',
+            },
+            modal: {
+              searchBox: {
+                resetButtonTitle: '清除查询条件',
+                resetButtonAriaLabel: '清除查询条件',
+                cancelButtonText: '取消',
+                cancelButtonAriaLabel: '取消',
+              },
+              startScreen: {
+                recentSearchesTitle: '搜索历史',
+                noRecentSearchesText: '没有搜索历史',
+                saveRecentSearchButtonTitle: '保存至搜索历史',
+                removeRecentSearchButtonTitle: '从搜索历史中移除',
+                favoriteSearchesTitle: '收藏',
+                removeFavoriteSearchButtonTitle: '从收藏中移除',
+              },
+              errorScreen: {
+                titleText: '无法获取结果',
+                helpText: '你可能需要检查你的网络连接',
+              },
+              footer: {
+                selectText: '选择',
+                navigateText: '切换',
+                closeText: '关闭',
+                searchByText: '搜索提供者',
+              },
+              noResultsScreen: {
+                noResultsText: '无法找到相关结果',
+                suggestedQueryText: '你可以尝试查询',
+                reportMissingResultsText: '你认为该查询应该有结果？',
+                reportMissingResultsLinkText: '点击反馈',
+              },
+            },
+          },
+        },
+      },
+    }),
+    googleAnalyticsPlugin({
+      id: process.env.DOCS_GA_ID ?? '',
+    }),
+    registerComponentsPlugin({
+      componentsDir: path.resolve(__dirname, './components'),
+    }),
+    // only enable shiki plugin in production mode
+    isProd ? shikiPlugin({ theme: 'dark-plus' }) : [],
+  ],
+})
