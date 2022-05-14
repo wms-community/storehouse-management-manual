59056248295fb1f24228298971ba3e3e6bf8b445
喵千寻 <101342651+Cinnamoroll-Rabbit@users.noreply.github.com>
2022-05-14 23:14:40 +0800

    Update en.ts

--- post/.vuepress/configs/navbar/en.ts
+++ post/.vuepress/configs/navbar/en.ts
@@ -5,17 +5,4 @@ export const en: NavbarConfig = [
     text: 'Guide',
     link: '/guide/',
   },
-  {
-    text: 'Reference',
-    children: [
-      {
-        text: 'VuePress',
-        children: [
-          {
-            text: 'CLI',
-            link: '/reference/cli.html',
-          },
-          '/reference/config.md',
-        ],
-      },
 ]
