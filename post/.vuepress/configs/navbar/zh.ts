e49706559b599f6ceda44c36fc5a3db17c396c5b
喵千寻 <101342651+Cinnamoroll-Rabbit@users.noreply.github.com>
2022-05-14 23:14:56 +0800

    Update zh.ts

--- post/.vuepress/configs/navbar/zh.ts
+++ post/.vuepress/configs/navbar/zh.ts
@@ -5,13 +5,4 @@ export const zh: NavbarConfig = [
     text: '指南',
     link: '/zh/guide/',
   },
-  {
-    text: '参考',
-    children: [
-      {
-        text: 'VuePress',
-        children: [
-          '/zh/reference/cli.md',
-        ],
-      },
 ]
