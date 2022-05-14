2dbfd6cf80d9a6fedcf3247cfded6b0c8a963e3f
喵千寻 <101342651+Cinnamoroll-Rabbit@users.noreply.github.com>
2022-05-14 22:15:34 +0800

    Create config.js

---
+++ post/.vuepress/config.js
@@ -0,0 +1,14 @@
+module.exports = {
+  locales: {
+    '/': {
+      lang: 'zh-CN',
+      title: 'Storehouse Management 用户手册',
+      description: 'Storehouse Management 用户手册',
+    },
+    '/en-us': {
+      lang: 'en-US',
+      title: 'Storehouse Management 用户手册',
+      description: 'Storehouse Management 用户手册',
+    },
+  },
+}
