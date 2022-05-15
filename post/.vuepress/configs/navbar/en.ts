01e3c57b0827e5a71d76f6a98399ac56b4d38a2b
喵千寻 <101342651+Cinnamoroll-Rabbit@users.noreply.github.com>
2022-05-15 22:14:39 +0800

    Delete post/.vuepress directory

--- post/.vuepress/configs/navbar/en.ts
+++
@@ -1,8 +0,0 @@
-import type { NavbarConfig } from '@vuepress/theme-default'
-
-export const en: NavbarConfig = [
-  {
-    text: 'Guide',
-    link: '/guide/',
-  },
-]
--- post/.vuepress/configs/navbar/zh.ts
+++
@@ -1,8 +0,0 @@
-import type { NavbarConfig } from '@vuepress/theme-default'
-
-export const zh: NavbarConfig = [
-  {
-    text: 'Storehouse Management 手册',
-    link: '/',
-  },
-]
